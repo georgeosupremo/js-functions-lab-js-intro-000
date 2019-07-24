d07153dbdbe72b40a4bb668b54998448fd1311f0
george.burton <george.burton@wework.com>
2019-07-24 22:50:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function happyHolidayTo(holiday, name){
   if(holiday, name)
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
+}
